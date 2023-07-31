a2d89da1ced44e627dd1e2a247661fbf5a64e819
EdwardMelendezM <192666@unsaac.edu.pe>
2023-07-30 22:26:15 -0500

    arreglando bugs

--- src/components/nav-mobile.tsx
+++ src/components/nav-mobile.tsx
@@ -1,25 +1,23 @@
-import { useState } from "preact/hooks";
-import menuProvider from "../providers/menu-provider";
 
 interface NavMobileProps{
   isActive:boolean
-  setIsActive:(state:boolean)=>void
+  onClose:()=>void
 }
 
-const NavMobile = ({ isActive, setIsActive }: NavMobileProps) => {
+const NavMobile = ({ isActive, onClose }: NavMobileProps) => {
   return (
     <>
       {
-        isActive && <div className="block lg:hidden fixed inset-0 bg-[rgba(255,255,255,0.9)] scroll-m-0" >
+        isActive && <div className="block lg:hidden fixed inset-0 bg-[rgba(255,255,255,0.95)] scroll-m-0" >
           <div className="flex justify-center items-center pt-20 flex-col gap-y-10 z-50">
             <h1 className="text-3xl">TuGuía</h1>
             <a href="/" className="hover:text-gray-600" >Home</a>
-            <a href="/" className="hover:text-gray-600" >Carreras</a>
-            <a href="/" className="hover:text-gray-600" >Historias</a>
-            <a href="/" className="hover:text-gray-600" >Comunidad</a>
+            <a href="/carreras" className="hover:text-gray-600" >Carreras</a>
+            <a href="/historias" className="hover:text-gray-600" >Historias</a>
+            <a href="/comunidad" className="hover:text-gray-600" >Comunidad</a>
           </div>
           <div
-            onClick={() => setIsActive(false)}
+            onClick={onClose}
             className="absolute top-10 right-10 hover:opacity-70 hover:cursor-pointer">
             <img src="/icons/close.svg" alt="" />
           </div>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,67 @@
+import { useState } from "preact/hooks";
+import { useRoutes } from "../hooks/useRoutes";
+import NavMobile from "./nav-mobile";
+
+interface NavbarProps{
+  pathname:string
+}
+
+const Navbar = ({ pathname }: NavbarProps) => {
+  const [isActive, setIsActive] = useState(false);
+  const routes = useRoutes(pathname);
+  const onClose = ()=>{
+    console.log(isActive)
+    setIsActive(false)
+  }
+  return ( 
+    <>
+      <NavMobile isActive={isActive} onClose={onClose} />
+      <nav class="w-screen flex px-6 py-8 justify-around">
+        
+        <div class="flex gap-x-3 justify-center items-center">
+          <img src="/images/logo.png" class="w-16" />
+          <a href="/" class="text-xl font-bold hover:text-gray-500 transition " > TuGuía </a>
+        </div>
+
+        <div class="hidden lg:flex gap-x-6 mt-4 ">
+          {
+            routes.map((item) => (
+              <a
+                href={`${item.path}`}
+                class={`${item.active && "font-bold"} text-lg hover:text-gray-500 transition`}>
+                {item.label}
+              </a>
+            ))
+          }
+        </div>
+        <div class="hidden lg:flex  gap-x-5" >
+          <a
+            href="/login"
+            class="mt-2 text-lg text-orange-500 font-medium border-none outline-none h-12 hover:text-orange-400 transition"
+          >
+            Iniciar session
+          </a>
+          <a
+            href="/register"
+            class="text-lg text-gray-100 font-medium border-none outline-none bg-orange-500 rounded-lg px-6 h-12 hover:bg-orange-400 transition flex items-center justify-center"
+          >
+            Registrarse
+          </a>
+        </div>
+        <div class="flex lg:hidden">
+          <div
+            className="flex items-center mx-auto">
+            <img
+              src="/icons/menu.svg"
+              alt="menu"
+              className="w-8 h-8 hover:cursor-pointer hover:opacity-60"
+              onClick={()=>setIsActive(true)}
+            />
+          </div>
+        </div>
+      </nav>
+    </>
+   );
+}
+ 
+export default Navbar;
